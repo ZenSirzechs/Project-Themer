a307e4973112ad0b79da66cbf9e6f0add0c3c904
nerd <me@n3rd3x3.xyz>
2022-03-21 20:33:24 -0500

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+ui_print "**********************************************"
+ui_print " PROJECT THEMER APP 2.0 ADDON "
+ui_print "                                              "
+ui_print "**********************************************"
+print_modname() {
+  ui_print "***********************************************"
+  ui_print "                  Installing..                   "
+  ui_print "***********************************************"
+  ui_print ""
+  ui_print "                   by @Drsants0                "
+  ui_print "***********************************************"
+}
